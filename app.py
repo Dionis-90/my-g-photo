6aa7fa2bed97cb91ed4bfe97c45a879a91939e92
Denis Shcherbina <denis-shch@bitbucket.org>
2020-09-18 19:22:04 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 
-import os, urllib.request, json, fileinput, re, subprocess
+import os, urllib.request, urllib.parse, json, fileinput, sqlite3
 from googleapiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -10,7 +10,8 @@ srv_endpoint = 'https://photoslibrary.googleapis.com/v1/'
 api_key = 'AIzaSyCk1qpI9w87PqlS1SgJlwdroAGYqHgZEEs'
 oauth2_file_path = 'storage-photos.json'
 identity_file_pth = 'client_id.json'
-dp_pth = 'db.sqlite'
+dbconn = sqlite3.connect('db.sqlite')
+c = dbconn.cursor()
 scopes = 'https://www.googleapis.com/auth/photoslibrary'
 
 ## AUTH
@@ -27,9 +28,6 @@ if not 'api_key' in globals():
 if not os.path.exists(oauth2_file_path):
     print(f"File {oauth2_file_path} does not exist!")
     exit()
-if not os.path.exists(dp_pth):
-    print(f"File {dp_pth} does not exist!")
-    exit()
 with open(oauth2_file_path) as oauth2_file:
     oauth2_file_data = json.load(oauth2_file)
     access_token = oauth2_file_data['access_token']
@@ -40,10 +38,9 @@ with open(oauth2_file_path) as oauth2_file:
 #token = {'Bearer ' access_token}
 #print(access_token)
 
-headers = {'Accept': 'application/json',
-           'Authorization': "Bearer {access_token}"}
 
-def refr_token():
+
+def refr_token() -> str:
     url = 'https://accounts.google.com/o/oauth2/token'
     values = {'client_id': client_id,
               'client_secret': client_secret,
@@ -57,9 +54,27 @@ def refr_token():
         the_page = response.read()
         #print(the_page)
         new_access_token = json.loads(the_page)['access_token']
-        print(f"New access_token {new_access_token} has been received.")
+        #print(f"New access_token {new_access_token} has been received.")
     for line in fileinput.input(oauth2_file_path, inplace=True):
         print (line.replace(access_token, new_access_token)),
+    return new_access_token
+
 
 #refr_token()
 
+def list_media_obj():
+    next_page_token = ''
+    headers = {'Accept': 'application/json',
+               'Authorization': 'Bearer ' + access_token,
+               'key': api_key, 
+               'pageSize': '10',
+               'pageToken': next_page_token}
+    #print(headers); exit()
+    #req = urllib.request.Request(srv_endpoint, data=None, headers)
+    #with urllib.request.urlopen(req) as response:
+        #json_resp = response.read()
+    #print(json_resp)
+    urllib.request.urlopen({srv_endpoint}, data=bytes(json.dumps(headers)), encoding="utf-8")
+
+
+list_media_obj()
