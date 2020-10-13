ee6b24a54591262e7f9798801ba12b67fd30b4d4
Denis <dionis201290@gmail.com>
2020-10-13 19:45:36 +0300

    Optimize.

--- app.py
+++ app.py
@@ -3,52 +3,55 @@
 import fileinput
 import json
 import os
+import requests
 import sqlite3
+import time
 import urllib.parse
 import urllib.request
-import requests
-
 from oauth2client import file, client, tools
 
-## Define vars and checks
+# Define global vars
 srv_endpoint = 'https://photoslibrary.googleapis.com/v1/'
 api_key = 'AIzaSyCk1qpI9w87PqlS1SgJlwdroAGYqHgZEEs'
 oauth2_file_path = 'storage-photos.json'
 identity_file_pth = 'client_id.json'
 dbconn = sqlite3.connect('db.sqlite')
-c = dbconn.cursor()
 scopes = 'https://www.googleapis.com/auth/photoslibrary'
 
-## AUTH
+# AUTH
 store = file.Storage(oauth2_file_path)
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets(identity_file_pth, scopes)
     creds = tools.run_flow(flow, store)
 
-## Checks
-if not 'api_key' in globals():
-    print(f"api_key - does not exits!")
-    exit()
+# Checks
 if not os.path.exists(oauth2_file_path):
     print(f"File {oauth2_file_path} does not exist!")
     exit()
-with open(oauth2_file_path) as oauth2_file:
-    oauth2_file_data = json.load(oauth2_file)
-    access_token = oauth2_file_data['access_token']
-    client_id = oauth2_file_data['client_id']
-    client_secret = oauth2_file_data['client_secret']
-    refresh_token = oauth2_file_data['refresh_token']
+def read_creds():
+    global access_token
+    global client_id
+    global access_token
+    global client_secret
+    global refresh_token
+    with open(oauth2_file_path) as oauth2_file:
+        oauth2_file_data = json.load(oauth2_file)
+        access_token = oauth2_file_data['access_token']
+        client_id = oauth2_file_data['client_id']
+        client_secret = oauth2_file_data['client_secret']
+        refresh_token = oauth2_file_data['refresh_token']
 
 
-def refr_token() -> str:
+read_creds()
+
+def refr_token():
     url = 'https://accounts.google.com/o/oauth2/token'
     values = {'client_id': client_id,
               'client_secret': client_secret,
               'refresh_token': refresh_token,
               'grant_type': 'refresh_token'}
     data = urllib.parse.urlencode(values)
-    #print(data); exit()
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
@@ -61,9 +64,7 @@ def refr_token() -> str:
     return new_access_token
 
 
-# refr_token()
-
-def list_media_obj(next_page_token) -> str:
+def list_media_obj(next_page_token):
     url = srv_endpoint+'mediaItems'
     headers = {'Accept': 'application/json',
                'Authorization': 'Bearer ' + access_token}
@@ -74,32 +75,34 @@ def list_media_obj(next_page_token) -> str:
     if r.status_code == 401:
         print('Refreshing token.')
         refr_token()
-        exit()
+        read_creds()
+        return 30
     the_page = r.text
-    next_page_token = json.loads(the_page)['nextPageToken']
+    new_next_page_token = json.loads(the_page)['nextPageToken']
     media_items = json.loads(the_page)['mediaItems']
     c = dbconn.cursor()
     for item in media_items:
-        item_id = (item['id'], )
         filename = item['filename']
-        # c.execute('SELECT filename FROM my_media WHERE object_id=?', item_id)
         values = (item['id'], item['filename'], item['mimeType'])
         try:
             c.execute('INSERT INTO my_media (object_id, filename, media_type) VALUES (?, ?, ?)', values)
         except sqlite3.IntegrityError:
             print(f'{filename} already exist.')
             dbconn.close()
-            return('10')
+            return 10
         except Exception:
             print('Unexpected error.')
             dbconn.close()
-            return('20')
+            return 20
         dbconn.commit()
-    dbconn.close()
-    # print(file_id)
-    # print(next_page_token)
-    return next_page_token
+    return new_next_page_token
 
-next_page_token = list_media_obj('')
-# print(next_page_token)
-# list_media_obj(next_page_token)
+
+list_media_obj_result = list_media_obj('')
+if list_media_obj_result == 30:
+    list_media_obj_result = list_media_obj('')
+while type(list_media_obj_result) == str:
+    list_media_obj_result = list_media_obj(list_media_obj_result)
+    time.sleep(2)
+dbconn.close()
+
