40686894569dfb3e00b1a6d7818f20388048e4c4
Denis <dionis201290@gmail.com>
2020-11-09 14:26:39 +0200

    Add logging.

--- app.py
+++ app.py
@@ -6,19 +6,28 @@ import os
 import requests
 import sqlite3
 import time
+import logging
 from oauth2client import file, client, tools
 
 # Define constants
 SRV_ENDPOINT = 'https://photoslibrary.googleapis.com/v1/'
 API_KEY = 'AIzaSyCk1qpI9w87PqlS1SgJlwdroAGYqHgZEEs'
-OAUTH2_FILE_PATH = 'storage-photos.json'
+OAUTH2_FILE_PATH = 'storage.json'
 IDENTITY_FILE_PATH = 'client_id.json'
 DB_FILE_PATH = 'db.sqlite'
 SCOPES = 'https://www.googleapis.com/auth/photoslibrary'
 PATH_TO_MEDIA_STORAGE = 'media/'
 
+logging.basicConfig(format='%(asctime)s %(message)s', filename='working.log', filemode='w', level=logging.INFO)
+
+logging.info('Started.')
+
 if not os.path.exists(IDENTITY_FILE_PATH):
-    print(f"File {IDENTITY_FILE_PATH} does not exist! Please put the file in working directory.")
+    logging.error(f"File {IDENTITY_FILE_PATH} does not exist! Please put the file in working directory.")
+    exit(1)
+
+if not os.path.exists(PATH_TO_MEDIA_STORAGE):
+    logging.error(f"File {PATH_TO_MEDIA_STORAGE} does not exist! Please set correct path.")
     exit(1)
 
 # AUTH
@@ -28,7 +37,7 @@ if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets(IDENTITY_FILE_PATH, SCOPES)
     creds = tools.run_flow(flow, store)
 if not os.path.exists(OAUTH2_FILE_PATH):
-    print(f"File {OAUTH2_FILE_PATH} does not exist! Authentication unsuccessful.")
+    logging.error(f"File {OAUTH2_FILE_PATH} does not exist! Authentication unsuccessful.")
     exit(1)
 
 
@@ -50,6 +59,7 @@ def refr_token():
     new_access_token = json.loads(response.text)['access_token']
     for line in fileinput.input(OAUTH2_FILE_PATH, inplace=True):
         print(line.replace(credentials['access_token'], new_access_token)),
+    logging.info('Token has been refreshed.')
     return new_access_token
 
 
@@ -71,12 +81,10 @@ def get_list_one_page(next_page_token):
               'pageToken': next_page_token}
     r = requests.get(url, params=params, headers=headers)
     if r.status_code == 401:
-        print('Refreshing token.')
         refr_token()
         return 30
-    the_page = r.text
-    new_next_page_token = json.loads(the_page)['nextPageToken']
-    media_items = json.loads(the_page)['mediaItems']
+    new_next_page_token = json.loads(r.text)['nextPageToken']
+    media_items = json.loads(r.text)['mediaItems']
     cur_db_connection = db_connect.cursor()
     for item in media_items:
         values = (item['id'], item['filename'], item['mimeType'])
@@ -84,10 +92,10 @@ def get_list_one_page(next_page_token):
             cur_db_connection.execute('INSERT INTO my_media (object_id, filename, media_type) \
             VALUES (?, ?, ?)', values)
         except sqlite3.IntegrityError:
-            print('List has been retrieved.')
+            logging.info('List has been retrieved.')
             return 10
         except Exception:
-            print('Unexpected error.')
+            logging.error('Unexpected error.')
             return 20
         db_connect.commit()
     return new_next_page_token
@@ -115,20 +123,20 @@ def get_media_files():
         elif 'video' in item[2]:
             r = requests.get(base_url+'=dv', params=None, headers=None)
         else:
-            print('Unexpected error.')
+            logging.error('Unexpected error.')
             return 1
 
         if 'text/html' in r.headers['Content-Type']:
-            print(r.text)
+            logging.warning(r.text)
             return 2
         elif 'image' in r.headers['Content-Type'] or 'video' in r.headers['Content-Type']:
             f = open(PATH_TO_MEDIA_STORAGE+item[1], 'wb')
             f.write(r.content)
             f.close()
         else:
-            print('Unexpected error.')
+            logging.error('Unexpected error.')
             return 3
-        print(f'{item[1]} - stored.')
+        logging.info(f'Item {item[1]} stored.')
         cur_db_connection.execute("UPDATE my_media SET loaded='1' WHERE object_id=?", (item[0],))
         db_connect.commit()
         time.sleep(2)
@@ -147,4 +155,4 @@ while type(result) == str:
 # Download media files to media folder.
 get_media_files()
 db_connect.close()
-print('Done.')
+logging.info('Finished.')
