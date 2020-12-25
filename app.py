09509563c3dc34162262663782560555442c6721
Denis <dionis201290@gmail.com>
2020-12-25 14:28:56 +0200

    Refactor and clean.

--- app.py
+++ app.py
@@ -1,86 +1,12 @@
 #!/usr/bin/env python3.7
 
-import json
-import os
-import requests
 import sqlite3
 import datetime
-import webbrowser
-from config import *
+from authorization import *
 
 # Define constants
 SRV_ENDPOINT = 'https://photoslibrary.googleapis.com/v1/'
-SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
-          'https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata',
-          'https://www.googleapis.com/auth/photoslibrary.sharing']
 DB = sqlite3.connect(DB_FILE_PATH)
-
-
-class Authorization:
-    def __init__(self):
-        with open(IDENTITY_FILE_PATH) as file_data:
-            self.identity_data = json.load(file_data)['installed']
-        scopes_for_uri = ''
-        for scope in SCOPES:
-            scopes_for_uri += scope+'%20'
-        self.url = f"{self.identity_data['auth_uri']}?scope={scopes_for_uri}&response_type=code&" \
-                   f"redirect_uri={self.identity_data['redirect_uris'][0]}&client_id={self.identity_data['client_id']}"
-        if not os.path.exists(OAUTH2_FILE_PATH):
-            auth_result = self.authenticate()
-            if auth_result != 0:
-                logging.error("Unexpected error.")
-                exit(1)
-        with open(OAUTH2_FILE_PATH) as f:
-            self.oauth2_data = json.load(f)
-        self.access_token = self.oauth2_data['access_token']
-
-    def authenticate(self) -> int:
-        print(f"If you do not have local browser please visit url: {self.url}")
-        webbrowser.open(self.url, new=0, autoraise=True)
-        code = input("Please enter the code: ")
-        data = {'code': code,
-                'client_id': self.identity_data['client_id'],
-                'client_secret': self.identity_data['client_secret'],
-                'redirect_uri': self.identity_data['redirect_uris'][0],
-                'grant_type': 'authorization_code'}
-        response = requests.post(self.identity_data['token_uri'], data=data)
-        if response.status_code != 200:
-            logging.error(f"Not authenticated. http code: {response.status_code}, response: {response.text}.")
-            return 1
-        with open(OAUTH2_FILE_PATH, 'w') as f:
-            json.dump(response.json(), f)
-        logging.info("Authenticated successfully.")
-        return 0
-
-    def get_access_token(self):
-        if os.path.exists(ACCESS_TOKEN_FILE):
-            with open(ACCESS_TOKEN_FILE) as f:
-                self.access_token = json.load(f)['access_token']
-        elif os.path.exists(OAUTH2_FILE_PATH):
-            with open(OAUTH2_FILE_PATH) as f:
-                self.access_token = json.load(f)['access_token']
-        else:
-            logging.error("Not authenticated.")
-            return 1
-        return self.access_token
-
-    def refresh_access_token(self):
-        values = {'client_id': self.identity_data['client_id'],
-                  'client_secret': self.identity_data['client_secret'],
-                  'refresh_token': self.oauth2_data['refresh_token'],
-                  'grant_type': 'refresh_token'}
-        response = requests.post(self.identity_data['token_uri'], data=values)
-        with open(ACCESS_TOKEN_FILE, 'w') as f:
-            json.dump(response.json(), f)
-        logging.info('Token has been refreshed.')
-
-
-class MediaItem:
-    pass
-
-
-class Mainloop:
-    pass
 
 
 def get_list_one_page(next_page_token, authorization, mode='fetch_all') -> tuple:
@@ -215,27 +141,6 @@ def get_media_files(authorization) -> int:
         cur_db_connection.execute("UPDATE my_media SET stored='1' WHERE object_id=?", (item[0],))
         DB.commit()
     return 0
-
-
-def list_albums():  # TODO
-    pass
-
-
-def create_album(album_name) -> str:  # TODO
-    print(album_name)
-    album_id = ''
-    return album_id
-
-
-def add_to_album(album_id, item_id) -> int:  # TODO
-    print(album_id, item_id)
-    return 0
-
-
-def share_album(album_id) -> str:  # TODO
-    print(album_id)
-    url = ''
-    return url
 
 
 def create_sub_folders_in_storage():
---
+++ authorization.py
@@ -0,0 +1,68 @@
+import json
+import os
+import webbrowser
+import requests
+from config import *
+
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
+          'https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata',
+          'https://www.googleapis.com/auth/photoslibrary.sharing']
+
+
+class Authorization:
+    def __init__(self):
+        with open(IDENTITY_FILE_PATH) as file_data:
+            self.identity_data = json.load(file_data)['installed']
+        scopes_for_uri = ''
+        for scope in SCOPES:
+            scopes_for_uri += scope+'%20'
+        self.url = f"{self.identity_data['auth_uri']}?scope={scopes_for_uri}&response_type=code&" \
+                   f"redirect_uri={self.identity_data['redirect_uris'][0]}&client_id={self.identity_data['client_id']}"
+        if not os.path.exists(OAUTH2_FILE_PATH):
+            auth_result = self.authenticate()
+            if auth_result != 0:
+                logging.error("Unexpected error.")
+                exit(1)
+        with open(OAUTH2_FILE_PATH) as f:
+            self.oauth2_data = json.load(f)
+        self.access_token = self.oauth2_data['access_token']
+
+    def authenticate(self) -> int:
+        print(f"If you do not have local browser please visit url: {self.url}")
+        webbrowser.open(self.url, new=0, autoraise=True)
+        code = input("Please enter the code: ")
+        data = {'code': code,
+                'client_id': self.identity_data['client_id'],
+                'client_secret': self.identity_data['client_secret'],
+                'redirect_uri': self.identity_data['redirect_uris'][0],
+                'grant_type': 'authorization_code'}
+        response = requests.post(self.identity_data['token_uri'], data=data)
+        if response.status_code != 200:
+            logging.error(f"Not authenticated. http code: {response.status_code}, response: {response.text}.")
+            return 1
+        with open(OAUTH2_FILE_PATH, 'w') as f:
+            json.dump(response.json(), f)
+        logging.info("Authenticated successfully.")
+        return 0
+
+    def get_access_token(self):
+        if os.path.exists(ACCESS_TOKEN_FILE):
+            with open(ACCESS_TOKEN_FILE) as f:
+                self.access_token = json.load(f)['access_token']
+        elif os.path.exists(OAUTH2_FILE_PATH):
+            with open(OAUTH2_FILE_PATH) as f:
+                self.access_token = json.load(f)['access_token']
+        else:
+            logging.error("Not authenticated.")
+            return 1
+        return self.access_token
+
+    def refresh_access_token(self):
+        values = {'client_id': self.identity_data['client_id'],
+                  'client_secret': self.identity_data['client_secret'],
+                  'refresh_token': self.oauth2_data['refresh_token'],
+                  'grant_type': 'refresh_token'}
+        response = requests.post(self.identity_data['token_uri'], data=values)
+        with open(ACCESS_TOKEN_FILE, 'w') as f:
+            json.dump(response.json(), f)
+        logging.info('Token has been refreshed.')
